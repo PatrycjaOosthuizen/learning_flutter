ed130549fbad232c036b361b342568aa18c75e0d
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-06-19 06:09:11 +0100

    Add audioplayers package

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +10,10 @@ class XylophoneApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: TextButton(onPressed :(){
+            final player = AudioCache();
+          }, child:Text('Click Me'))
+          ),
         ),
       ),
     );
