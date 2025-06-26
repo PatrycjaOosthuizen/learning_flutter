eddda76334031a2e35666471ecd81cc7f81fd9ef
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-06-26 07:18:22 +0100

    Extract ReusableCard widget to reduce code duplication and improve layout structure
    - Created a ReusableCard stateless widget with common styling
    - Replaced repeated Container widgets with ReusableCard
    - Improves code readability and maintainability

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -16,13 +16,7 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget> [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),),
+              Expanded(child: ReusableCard(),),
               Expanded(child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
@@ -32,32 +26,31 @@ class _InputPageState extends State<InputPage> {
               ),),
             ],
           ),),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),),
+          Expanded(child: ReusableCard()),
           Expanded(child: Row(
             children: <Widget> [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),),
+              Expanded(child: ReusableCard()),
+              Expanded(child: ReusableCard()),
             ],
           ),),
         ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(8.0),
       ),
     );
   }
