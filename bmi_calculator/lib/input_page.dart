09e287aed33e204d34ae8daa72b706ac4b7120ba
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-06-26 14:59:15 +0100

    Fix errors and add gender selection cards with FontAwesome icons

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
 const bottomContainerColor = Color(0XFFEB1555);
 
 class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -14,38 +17,46 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: Text('BMI CALCULATOR', style: TextStyle(color: Colors.white)),
       ),
       body: Column(
         children: <Widget>[
-          Expanded(child: Row(
-            children: <Widget> [
-              Expanded(child: ReusableCard(colour: activeCardColor,),
-              ),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: activeCardColor,
-                  borderRadius: BorderRadius.circular(8.0),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                  ),
                 ),
-              ),),
-            ],
-          ),),
-          Expanded(child: ReusableCard(colour: activeCardColor,),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Expanded(child: Row(
-            children: <Widget> [
-              Expanded(child: ReusableCard(colour: activeCardColor,),
-              ),
-              Expanded(child: ReusableCard(colour: activeCardColor,),
-              ),
-            ],
-          ),),
+          Expanded(child: ReusableCard(colour: activeCardColor)),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(colour: activeCardColor)),
+                Expanded(child: ReusableCard(colour: activeCardColor)),
+              ],
+            ),
+          ),
           Container(
             color: bottomContainerColor,
-            margin: EdgeInsets.only(top:10.0),
+            margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: bottomContainerHeight,
           ),
@@ -55,8 +66,27 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent({required this.icon, required this.label, super.key});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, size: 80.0, color: Colors.white),
+        SizedBox(height: 15.0),
+        Text(label, style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98))),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({ required this.colour, this.cardChild, super.key});
+  const ReusableCard({required this.colour, this.cardChild, super.key});
 
   final Color colour;
   final Widget? cardChild;
@@ -64,12 +94,12 @@ class ReusableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(8.0),
       ),
+      child: cardChild,
     );
   }
 }
