4c626efd22e726e94dc5aecd29bd3a3fce58df6f
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-07-01 06:25:10 +0100

    refactor: upgrade ReusableCard to handle touch using an onPress callback

--- bmi_calculator/lib/reusable_card.dart
+++ bmi_calculator/lib/reusable_card.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({required this.colour, this.cardChild, super.key});
+  const ReusableCard({required this.colour, this.cardChild, this.onPress, super.key});
 
   final Color colour;
   final Widget? cardChild;
+  final VoidCallback? onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(8.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
