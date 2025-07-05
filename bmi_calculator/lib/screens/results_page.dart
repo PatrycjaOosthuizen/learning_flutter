de2907ae10d46f8d32911e05d38d0c7e3bff1d2f
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-07-05 08:28:18 +0100

    Add RE-CALCULATE BottomButton to navigate back from ResultsPage

--- bmi_calculator/lib/screens/results_page.dart
+++ bmi_calculator/lib/screens/results_page.dart
@@ -1,7 +1,9 @@
 import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
-import '../components/reusable_card.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+
+
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
@@ -37,6 +39,9 @@ class ResultsPage extends StatelessWidget {
               ) ,
             ),
           ),
+          BottomButton(onTap: (){
+            Navigator.pop(context);
+          }, buttonTitle: 'RE-CALCULATE')
         ],
       ),
     );
