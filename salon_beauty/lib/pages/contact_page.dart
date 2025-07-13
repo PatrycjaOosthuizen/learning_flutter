584f1621af2e49cd565c2aad0050ddcb324306d9
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-07-13 07:14:09 +0100

    Add contact page with simple content

---
+++ salon_beauty/lib/pages/contact_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
+import '../widgets/navbar.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Navbar(),
+            const SizedBox(height: 20),
+            Text(
+              'Kontakt',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: AppColors.brown,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Zadzwoń: +48 123 456 789\nEmail: kontakt@salon.pl\nAdres: ul. Piękna 12, Warszawa',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
