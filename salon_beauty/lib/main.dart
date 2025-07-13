52d39b7cac0fa64c665d960dac5ed2800756961e
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-07-13 07:18:08 +0100

    Update and import the pages to  main.dart with routes to about, gallery and contact pages

--- salon_beauty/lib/main.dart
+++ salon_beauty/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/services_page.dart';
+import 'pages/about_page.dart';
+import 'pages/gallery_page.dart';
+import 'pages/contact_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +24,9 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomePage(),
         '/services': (context) => const ServicesPage(),
+        '/about': (context) => const AboutPage(),
+        '/gallery': (context) => const GalleryPage(),
+        '/contact': (context) => const ContactPage(),
       },
     );
   }
